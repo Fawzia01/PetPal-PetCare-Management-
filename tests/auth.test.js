67aaa174322969e3b8c555b887d858f613b2c25a
Shahinur-Begum <bshahinur256@gmail.com>
2025-12-02 07:20:24 +0600

    Add files via upload

---
+++ tests/auth.test.js
@@ -0,0 +1,21 @@
+import request from "supertest";
+import app from "../index.js"; // your main file
+
+describe("User Auth", () => {
+  it("signup and login", async () => {
+    // Signup
+    await request(app).post("/signup").send({
+      name: "Test",
+      email: "test@example.com",
+      password: "123456"
+    });
+
+    // Login
+    const res = await request(app).post("/login").send({
+      email: "test@example.com",
+      password: "123456"
+    });
+
+    console.log(res.body); // See token
+  });
+});
